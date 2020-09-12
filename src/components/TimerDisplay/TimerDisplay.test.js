2413afdd537cbc9232e244059e5d32957b46bde2
Nick Hart <nick.hart.des@gmail.com>
2020-09-11 22:36:30 -0500

    Add initial test for timer

--- src/components/TimerDisplay/TimerDisplay.test.js
+++ src/components/TimerDisplay/TimerDisplay.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import TimerDisplay from './TimerDisplay'
 import '@testing-library/jest-dom'
 
 describe('TimerDisplay', () => {
-  it('should render without crashing', () => {
-    
+  it('should start at 59 seconds on render', () => {
+    render(
+      <TimerDisplay />
+    )
+
+    const timer = screen.getByText('59')
+
+    expect(timer).toBeInTheDocument()
   })
 })
