2fe634758e10ac8e53aad552f7e673cf7230e7ec
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-12 12:21:41 -0600

    Edit it block in first test #13

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom'
 import App from './App';
 
-test('renders learn react link', () => {
+test('should display the header when rendered', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
