cfa4fffb743f5e988f6a28b80a36c533f65acab7
Nick Hart <nick.hart.des@gmail.com>
2020-09-11 20:56:59 -0500

    Add initial render method

--- src/components/StoryEditView/StoryEditView.js
+++ src/components/StoryEditView/StoryEditView.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './StoryEditView.css'
 
 class StoryEditView extends Component {
   constructor() {
-    super();
+    super()
+  }
+
+  render() {
+    return (
+      <h1>Story Edits</h1>
+    )
   }
 }
 
