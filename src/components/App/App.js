bf8ef6bda33d80bc6efe37ddd2830d0fa403c1c7
Nick Hart <nick.hart.des@gmail.com>
2020-09-11 20:47:47 -0500

    Add router back in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
+import WelcomePageView from '../WelcomePageView/WelcomePageView'
+import StorySetupView from '../StorySetupView/StorySetupView'
+import StoryEditView from '../StoryEditView/StoryEditView'
+import LibraryView from '../LibraryView/LibraryView'
+import PublishedStoryView from '../PublishedStoryView/PublishedStoryView'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Route
+        exact path='/'
+        render={() => {
+          return <WelcomePageView />
+        }}
+      />
+      <Route
+        exact path='/story-setup'
+        render={() => {
+          return <StorySetupView />
+        }}
+      />
+      <Route
+        exact path='/story-edit'
+        render={() => {
+          return <StoryEditView />
+        }}
+      />
+      <Route
+        exact path='/library'
+        render={() => {
+          return <LibraryView />
+        }}
+      />
+      <Route
+        exact path='/published-story/:id'
+        render={() => {
+          return <PublishedStoryView />
+        }}
+      />
+    </main>
+  )
 }
 
 export default App;
