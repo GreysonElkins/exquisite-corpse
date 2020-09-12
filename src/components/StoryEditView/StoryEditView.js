804f5defea9be3b813e8a175ae2c06fb6dd3e6a6
Nick Hart <nick.hart.des@gmail.com>
2020-09-11 22:48:22 -0500

    Add updateText

--- src/components/StoryEditView/StoryEditView.js
+++ src/components/StoryEditView/StoryEditView.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import TimerDisplay from '../TimerDisplay/TimerDisplay'
 import './StoryEditView.css'
 
 class StoryEditView extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {}
+  }
+
+  updateText = e => {
+    this.setState({ [e.target.id]: e.target.value })
   }
 
   render() {
     return (
       <main id="story-edit-container">
         <h1 id="prompt">Prompt placeholder</h1>
+        <TimerDisplay />
         <input
-          id="title-input"
+          id="title"
           type="text"
+          onChange={this.updateText}
           placeholder="Enter your title here"
         />
         <input
-          id="story-input"
+          id="story"
           type="textarea"
+          onChange={this.updateText}
           placeholder="Type your story here"
         />
         <button type="button">Post</button>
-        <TimerDisplay />
       </main>
     );
   }
