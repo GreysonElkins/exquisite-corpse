8f8f5e2d4ed96dd7e902e7134cf076e537d93310
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-12 12:22:18 -0600

    Begin file setup and pause to build lower level component #13

---
+++ src/components/StoryEditView/StoryEditView.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import StoryView from "./StoryView";
---
+++ src/components/StorySetupView/StorySetupView.js
@@ -0,0 +1,6 @@
+// import React from 'react'
+// import StorySetup from '../StorySetupView'
+
+// export default StorySetupView;
+
+// just needs to render the StorySetup component
