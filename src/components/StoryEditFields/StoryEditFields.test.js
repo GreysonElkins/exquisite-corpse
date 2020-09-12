5f82b8569e76cb0ac46ca8e8e3613c8b8077478e
Nick Hart <nick.hart.des@gmail.com>
2020-09-12 13:17:10 -0500

    Add initial StoryEditFields test

--- src/components/StoryEditFields/StoryEditFields.test.js
+++ src/components/StoryEditFields/StoryEditFields.test.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import StoryEditFields from './StoryEditFields'
 import '@testing-library/jest-dom'
 
 describe('StoryEditFields', () => {
   it('should render a header and two input fields', () => {
-    
+    render(
+      <Router>
+        <StoryEditFields />
+      </Router>
+    )
+
+    const header = screen.getByText(/prompt placeholder/i)
+    const titleInputField = screen.getByPlaceholderText(/enter your title here/i)
+    const storyInputField = screen.getByPlaceholderText(/type your story here/i)
+
+    expect(header).toBeInTheDocument()
+    expect(titleInputField).toBeInTheDocument()
+    expect(storyInputField).toBeInTheDocument()
   })
 })
