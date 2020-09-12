4d560b046c2d5826b86669dd8850dad1c0d63765
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-11 22:18:37 -0600

    Add navbar with navlink components #19

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Navlink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
-      <h1>Exquisite Corpse</h1>
+      <Link><h1>Exquisite Corpse</h1></Link>
+      <nav className='navbar'>
+        <NavLink to='/story-setup'><button>New Story</button></NavLink>
+        <NavLink to='/library'><button>Library</button></NavLink>
+        <NavLink to='/login'><button>Sign In</button></NavLink>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header
