5d53b92428fed3d9905131b3a9f70d2b9351b10d
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-11 21:24:32 -0600

    Comment out unused routes for development #19

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom'
+import Header from '../Header/Header'
 import WelcomePageView from '../WelcomePageView/WelcomePageView'
 import StorySetupView from '../StorySetupView/StorySetupView'
 import StoryEditView from '../StoryEditView/StoryEditView'
@@ -11,36 +12,37 @@ import PublishedStoryView from '../PublishedStoryView/PublishedStoryView'
 function App() {
   return (
     <main>
-      <Route 
+      <Header />
+      {/* <Route 
         exact path='/' 
         render={ () => {
           return <WelcomePageView /> 
         }}
-      />
+      /> */}
       <Route 
         exact path='/story-setup' 
         render={ () => {
           return <StorySetupView /> 
         }}
       />
-      <Route 
+      {/* <Route 
         exact path='/story-edit' 
         render={ () => {
           return <StoryEditView /> 
         }}
-      />  
-      <Route 
+      />   */}
+      {/* <Route 
         exact path='/library' 
         render={ () => {
           return <LibraryView /> 
         }}
-      />
-      <Route 
+      /> */}
+      {/* <Route 
         exact path='/published-story/:id' 
         render={ () => {
           return <PublishedStoryView /> 
         }}
-      />  
+      />   */}
     </main>
   )
 }
