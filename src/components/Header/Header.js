b274bab08a1f6acd24d22aeeb4dce9404886226d
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-11 21:24:54 -0600

    Create initial component structure #19

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navlink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Exquisite Corpse</h1>
+    </header>
+  )
+}
+
+export default Header
