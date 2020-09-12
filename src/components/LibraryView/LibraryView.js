5428c989134053a4038f3000141679e239f7d766
Greyson Elkins <greysonelkins@gmail.com>
2020-09-11 20:10:20 -0600

    Make Library component

---
+++ src/components/LibraryView/LibraryView.js
@@ -0,0 +1,12 @@
+import React, { component } from 'react'
+
+class Library extends component {
+  constructor() {
+    super()
+    this.state = {
+      stories: []
+    }
+  }
+}
+
+export default Library
