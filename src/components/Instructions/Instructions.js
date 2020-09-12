42a3b3545688a8371586b81368bc9f70d498528a
Carly Clift <carlymclift@gmail.com>
2020-09-12 13:35:15 -0600

    Move game history to own component

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Instructions.css'
+import GameHistory from '../GameHistory/GameHistory'
 import { Link } from 'react-router-dom'
 
 const Instructions = ( { displayHistory, showHistory } ) => {
@@ -17,23 +18,8 @@ const Instructions = ( { displayHistory, showHistory } ) => {
           </ol>
         </ul>
       </div>
-      <div className="drop-arrow-sec"><button className="drop-arrow-button" onClick={displayHistory}></button><p className="show-history-text">History of the Game</p></div>
-      {showHistory &&
-        <div className="game-history-box">
-          <p>
-             Exquisite Corpse, also known as <i>Exquisite Cadaver</i> is a game that gained popularity in artistic circles during the 1920s when it was 
-             adopted as a technique by artists of the Surrealist movement to generate collaborative compositions. 
-             The name comes from the phase <q>Le cadavre exquis boira le vin nouveau.</q> ("The exquisite corpse shall drink the new wine.")
-             Exquisite Corpse very is similar to the parlour game <i>Consequences</i>.
-             The original game consisted of participants taking turns writing or drawing on a sheet of paper, 
-             folding it to conceal his or her contribution (except a small portion), and then passing it to the next player for a further contribution.
-             The game started as a fun way to pass time but later became an enriching experience for artists, writers and students alike.
-          </p>
-          <div className="game-history-drawings">
-            
-          </div>
-        </div>
-      }
+      <button className="show-history-button" onClick={displayHistory}>History of the Game</button>
+      {showHistory ? <GameHistory displayHistory={displayHistory}/> : null}
       <h3>Continue a story:</h3>
       <Link to='/story-setup'>Start a new story</Link>
     </section>
