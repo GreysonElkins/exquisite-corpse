dfc064d60220efae9e6a0d925e8716cb06d29bf4
Nick Hart <nick.hart.des@gmail.com>
2020-09-11 20:50:40 -0500

    Add StoryEditView boilerplate

---
+++ src/components/StoryEditView/StoryEditView.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class StoryEditView extends Component {
+  constructor() {
+    super();
+  }
+}
+
+export default StoryEditView;
