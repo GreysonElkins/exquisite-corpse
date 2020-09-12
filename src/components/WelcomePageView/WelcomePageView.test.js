a170bb42f84f05da69db5237778bbcb2c6fe0519
Carly Clift <carlymclift@gmail.com>
2020-09-12 01:09:32 -0600

    Add test for firing the showHistory method

--- src/components/WelcomePageView/WelcomePageView.test.js
+++ src/components/WelcomePageView/WelcomePageView.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import WelcomePageView from './WelcomePageView'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 import ApiHelper from '../../ApiHelpers/apiCalls'
@@ -15,7 +15,47 @@ describe('WelcomePageView', () => {
     )
     
     const heading = screen.getByText('Rules of Play')
-
+    const button = screen.getByRole('button', {name: '>'})
+    const bulletPoint = screen.getByText('Exquisite Corpse is a collaborative', { exact: false })
+    const instrOne = screen.getByText('Select a new prompt', { exact: false })
+    const instrTwo = screen.getByText('Start your section', { exact: false })
+    const instrThree = screen.getByText('Be creative and write fast', { exact: false })
+    
     expect(heading).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+    expect(bulletPoint).toBeInTheDocument()
+    expect(instrOne).toBeInTheDocument()
+    expect(instrTwo).toBeInTheDocument()
+    expect(instrThree).toBeInTheDocument()
+  })
+
+  it('Should fire method when button is clicked', () => {
+    const mockFun = jest.fn()
+
+    render(
+      <MemoryRouter>
+        <WelcomePageView />
+      </MemoryRouter>
+    )
+
+    const button = screen.getByRole('button', {name: '>'})
+    fireEvent.click(button)
+
+    expect(mockFun).toBeCalled()
+  })
+
+  it('Should render history when state changes', () => {
+    render(
+      <MemoryRouter>
+        <WelcomePageView 
+         showHistory={true}
+        />
+      </MemoryRouter>
+    )
+
+    const hiddenHistory = screen.getByText('Exquisite Corpse, also known', { exact: false })
+
+    expect(hiddenHistory).toBeInTheDocument()
+
   })
 })
