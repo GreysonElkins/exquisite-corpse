2014f1fdb2ce627024fc5871c3699164f30ff2df
Carly Clift <carlymclift@gmail.com>
2020-09-12 17:43:01 -0600

    Test that history card text renders

--- src/components/GameHistory/GameHistory.test.js
+++ src/components/GameHistory/GameHistory.test.js
@@ -13,10 +13,22 @@ describe('GameHistory', () => {
     )
     
     const button = screen.getByRole('close-pop-up')
-    const bulletPoint = screen.getByText('le cadavre exquis boira le vin nouveau.', { exact: false })
-    
+    const text = screen.getByText('le cadavre exquis boira le vin nouveau.', { exact: false })
+    const date1 = screen.getByText(/1929/i)
+    const date2 = screen.getByText(/1929/i)
+    const date3 = screen.getByText(/1929/i)
+    const date4 = screen.getByText(/1929/i)
+    const date5 = screen.getByText(/1929/i)
+    const date6 = screen.getByText(/1929/i)
+
     expect(button).toBeInTheDocument()
-    expect(bulletPoint).toBeInTheDocument()
+    expect(text).toBeInTheDocument()
+    expect(date1).toBeInTheDocument()
+    expect(date2).toBeInTheDocument()
+    expect(date3).toBeInTheDocument()
+    expect(date4).toBeInTheDocument()
+    expect(date5).toBeInTheDocument()
+    expect(date6).toBeInTheDocument()
   })
 
   it('Should fire a method when the close button is clicked', () => {
