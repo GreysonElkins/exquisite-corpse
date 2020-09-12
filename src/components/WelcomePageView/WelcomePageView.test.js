028a0155c22bc6c7b893675e4b8740c026752d0a
Carly Clift <carlymclift@gmail.com>
2020-09-12 00:39:00 -0600

    Pass first unit test

--- src/components/WelcomePageView/WelcomePageView.test.js
+++ src/components/WelcomePageView/WelcomePageView.test.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import WelcomePageView from './WelcomePageView'
-import { screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
-import ApiHelper from '../ApiHelpers/apiCalls'
-jest.mock('../ApiHelpers/apiCalls')
+import ApiHelper from '../../ApiHelpers/apiCalls'
+jest.mock('../../ApiHelpers/apiCalls')
 
 describe('WelcomePageView', () => {
   it('Should have the correct content when rendered', () => {
+    render(
+      <MemoryRouter>
+        <WelcomePageView />
+      </MemoryRouter>
+    )
     
+    const heading = screen.getByText('Rules of Play')
+
+    expect(heading).toBeInTheDocument()
   })
 })
