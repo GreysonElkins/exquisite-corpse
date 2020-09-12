3f4e94d248855e7bcd8a639681704e800fd82e73
Greyson Elkins <greysonelkins@gmail.com>
2020-09-12 12:49:02 -0600

    Move book to it's own component

---
+++ src/components/Bookshelf/Book.js
@@ -0,0 +1,38 @@
+import React from "react";
+import moment from "moment";
+import "./Bookshelf.scss";
+
+import book1 from "../../assets/bookspine1.png";
+import book2 from "../../assets/bookspine2.png";
+import book3 from "../../assets/bookspine3.png";
+import book4 from "../../assets/bookspine4.png";
+import book5 from "../../assets/bookspine5.png";
+import book6 from "../../assets/bookspine6.png";
+import book7 from "../../assets/bookspine7.png";
+
+const bookSpines = [book1, book2, book3, book4, book5, book6, book7];
+
+const getBookSpine = () => {
+  const random = Math.floor(Math.random() * Math.floor(5));
+  return bookSpines[random];
+};
+
+const Book = ({ story, onClick}) => {
+  const spine = getBookSpine();
+  return (
+    <div 
+      className="book" 
+      style={{ backgroundImage: `url(${spine})` }}
+      role="button"
+      onClick={onClick}
+    >
+      <span className="title">{story.title}</span>
+      <span className="prompt">{story.prompt}</span>
+      <span className="date-published">
+        {moment(story.updated_at).format("MMM YYYY")}
+      </span>
+    </div>
+  );
+}
+
+export default Book
--- src/components/Bookshelf/Bookshelf.js
+++ src/components/Bookshelf/Bookshelf.js
@@ -1,40 +1,12 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import moment from 'moment'
+import Book from './Book'
 import './Bookshelf.scss'
-import book1 from "../../assets/bookspine1.png";
-import book2 from "../../assets/bookspine2.png";
-import book3 from "../../assets/bookspine3.png";
-import book4 from "../../assets/bookspine4.png";
-import book5 from "../../assets/bookspine5.png";
-import book6 from "../../assets/bookspine6.png";
-import book7 from "../../assets/bookspine7.png";
-
-
-const bookSpines = [book1, book2, book3, book4, book5, book6, book7]
-
-const getBookSpine = () => {
-  const random = Math.floor(Math.random() * Math.floor(5));
-  return bookSpines[random]
-}
 
 const Bookshelf = (props) => {
   const booksSelection = props.stories.map((story, i) => {
-    const spine = getBookSpine(i)
+    
     return (
-    <Link 
-      to={story.isComplete ? `/published-story/${story.id}` : `/story-edit/${story.id}`}
-      key={`book${i}`}
-      className="book-link"
-    >
-      <span className='book' style={{backgroundImage: `url(${spine})`}}>
-        <div className="title">{story.title}</div>
-        <div className="prompt">{story.prompt}</div>
-        <div className="date-published">{
-          moment(story.updated_at).format('MMM YYYY')
-        }</div>
-      </span>
-    </Link>
+      <Book story={story} key={i}/>
     )
   })
     
