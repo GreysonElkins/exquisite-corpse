f6698166167523bf7f7a4a2baca9914ac76a1341
Greyson Elkins <greysonelkins@gmail.com>
2020-09-12 12:32:03 -0600

    Render library on route / ignore .env

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
 import WelcomePageView from '../WelcomePageView/WelcomePageView'
@@ -8,41 +7,43 @@ import StoryEditView from '../StoryEditView/StoryEditView'
 import LibraryView from '../LibraryView/LibraryView'
 import PublishedStoryView from '../PublishedStoryView/PublishedStoryView'
 
-function App() {
-  return (
-    <main>
-      <Route 
-        exact path='/' 
-        render={ () => {
-          return <WelcomePageView /> 
-        }}
-      />
-      <Route 
-        exact path='/story-setup' 
-        render={ () => {
-          return <StorySetupView /> 
-        }}
-      />
-      <Route 
-        exact path='/story-edit' 
-        render={ () => {
-          return <StoryEditView /> 
-        }}
-      />  
-      <Route 
-        exact path='/library' 
-        render={ () => {
-          return <LibraryView /> 
-        }}
-      />
-      <Route 
-        exact path='/published-story/:id' 
-        render={ () => {
-          return <PublishedStoryView /> 
-        }}
-      />  
-    </main>
-  )
+class App extends Component {
+  render() {
+    return (
+      <main>
+        {/* <Route 
+          exact path='/' 
+          render={ () => {
+            return <WelcomePageView /> 
+          }}
+        />
+        <Route 
+          exact path='/story-setup' 
+          render={ () => {
+            return <StorySetupView /> 
+          }}
+        />
+        <Route 
+          exact path='/story-edit' 
+          render={ () => {
+            return <StoryEditView /> 
+          }}
+        />   */}
+        <Route 
+          exact path='/library' 
+          render={ () => {
+            return <LibraryView /> 
+          }}
+        />
+        {/* <Route 
+          exact path='/published-story/:id' 
+          render={ () => {
+            return <PublishedStoryView /> 
+          }}
+        />   */}
+      </main>
+    )
+  }
 }
 
 export default App;
