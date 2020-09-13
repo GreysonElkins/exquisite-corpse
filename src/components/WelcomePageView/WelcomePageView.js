94e8c1632b3010117485c11b2f19069b22b23fc2
Carly Clift <carlymclift@gmail.com>
2020-09-13 14:55:25 -0600

    Delete prompt cards

--- src/components/WelcomePageView/WelcomePageView.js
+++ src/components/WelcomePageView/WelcomePageView.js
@@ -2,8 +2,6 @@ import React, { Component } from "react"
 import './WelcomePageView.css'
 import ApiHelper from '../../ApiHelper/ApiHelper'
 import Instructions from '../Instructions/Instructions'
-import LibraryView from '../LibraryView/LibraryView'
-import PromptCards from '../PromptCards/PromptCards'
 import Bookshelf from "../Bookshelf/Bookshelf"
 import { Redirect } from 'react-router-dom'
 
@@ -19,6 +17,7 @@ class WelcomePageView extends Component {
       currentPage: 'home'
     }
   }
+
   componentDidMount() {
     const unCompletedStories = []
     ApiHelper.getData().then(stories => {
@@ -29,17 +28,7 @@ class WelcomePageView extends Component {
         }
       })
     })
-
-    /// just test info, will be chnaged after api is hooked up
-    this.setState({ inProgressStories: [
-        "It was a dark and stormy party and suddenly the doorbell rang...",
-        "The warp core is putting off unusual photon emissions...",
-        "With a wave of the wand, the wizard cast a spell and suddenly...",
-        "There is a suspicious trail of cheetos leading down this hall...",
-        "For the first time in over 35 years, the vault doors groaned as they slowly creaked open..."
-     ]
-    })
-   }
+  }
 
   displayHistory = () => {
     this.setState(prevState => {
@@ -74,11 +63,6 @@ class WelcomePageView extends Component {
               displayHistory={this.displayHistory}
             />
           </div>
-          {/* <div>
-            <PromptCards 
-              inProgressStories={this.state.inProgressStories}
-            />
-          </div> */}
           <Bookshelf
             stories={this.state.stories}
             onClick={this.takeToWritingSection}
