04c5dfc96986b18133b5bb0aa95b641baeede35c
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-12 23:48:51 -0600

    Remove setup #13

--- src/components/StoryEditView/StoryEditView.test.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import "@testing-library/jest-dom";
-import StoryView from "./StoryView";
