4a70c773543938d6d0473befec00ea2d17a982c3
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-13 20:43:45 -0600

    Add specific classnames #38

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,19 @@ import './Header.scss'
 
 const Header = () => {
   return (
-    <header>
-      <Link to='/'><h1>Exquisite Corpse</h1></Link>
-      <nav className='navbar'>
-        <NavLink to='/story-setup' activeClassName='selected'>
-          <button>New Story</button>
+    <header className="header">
+      <Link to="/">
+        <h1 className='title'>Exquisite Corpse</h1>
+      </Link>
+      <nav className="navbar">
+        <NavLink to="/story-setup" activeClassName="selected">
+          <button className="nav-button">New Story</button>
         </NavLink>
-        <NavLink to='/library' activeClassName='selected'>
-          <button>Library</button>
+        <NavLink to="/library" activeClassName="selected">
+          <button className="nav-button">Library</button>
         </NavLink>
-        <NavLink to='/login' activeClassName='selected'>
-          <button>Sign In</button>
+        <NavLink to="/login" activeClassName="selected">
+          <button className="nav-button">Sign In</button>
         </NavLink>
       </nav>
     </header>
