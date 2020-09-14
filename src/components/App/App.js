12ef666590be42e96767a226483689ebe7e3671e
Greyson Elkins <greysonelkins@gmail.com>
2020-09-14 15:10:16 -0600

    Fetch stories and prompts onload

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,38 @@ import WelcomePageView from '../WelcomePageView/WelcomePageView'
 import StorySetupView from '../StorySetupView/StorySetupView'
 import StoryEditView from '../StoryEditView/StoryEditView'
 import LibraryView from '../LibraryView/LibraryView'
+import ApiHelper from '../../ApiHelper/ApiHelper';
 
 class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      stories: [],
+      prompts: [],
+      authors: []
+    }
+  }
+
+  componentDidMount() {
+    ApiHelper.getData('prompts')
+      .then(allPrompts => {
+        this.setState({prompts: allPrompts})
+
+      })
+      .then(() => {
+        ApiHelper.getData('stories').then(allStories => {
+          const updatedStories = []
+          allStories.forEach(story => {
+            if (story.prompt) {
+              story.prompt = this.state.prompts.find(prompt => prompt.id === story.prompt)
+            }
+            updatedStories.push(story)
+          })
+          this.setState({ stories: updatedStories })
+        })
+      })
+  }
+
   render() {
     return (
       <main>
