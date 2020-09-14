253dda924c70f7b5da8a3ea80b5c776bbcdda9b4
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-13 20:45:41 -0600

    Change Double quotes to singles #38

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,23 @@ import './Header.scss'
 
 const Header = () => {
   return (
-    <header className="header">
-      <Link to="/">
+    <header className='header'>
+      <Link to='/'>
         <h1 className='title'>Exquisite Corpse</h1>
       </Link>
-      <nav className="navbar">
-        <NavLink to="/story-setup" activeClassName="selected">
-          <button className="nav-button">New Story</button>
+      <nav className='navbar'>
+        <NavLink to='/story-setup' activeClassName='selected'>
+          <button className='nav-button'>New Story</button>
         </NavLink>
-        <NavLink to="/library" activeClassName="selected">
-          <button className="nav-button">Library</button>
+        <NavLink to='/library' activeClassName='selected'>
+          <button className='nav-button'>Library</button>
         </NavLink>
-        <NavLink to="/login" activeClassName="selected">
-          <button className="nav-button">Sign In</button>
+        <NavLink to='/login' activeClassName='selected'>
+          <button className='nav-button'>Sign In</button>
         </NavLink>
       </nav>
     </header>
-  );
+  )
 }
 
 export default Header
