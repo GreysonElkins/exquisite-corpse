7f8fc54cd464ebc52dd9fb19ce3c79d6912ef142
Nick Hart <nick.hart.des@gmail.com>
2020-09-14 17:30:59 -0500

    Remove getRandomPrompt

--- src/ApiHelper/ApiHelper.js
+++ src/ApiHelper/ApiHelper.js
@@ -6,21 +6,6 @@ class ApiHelper {
     return fetch(`${apiHead}/${type}/${id ? id : ''}`)
       .then(response => response.json())
   }
-
-  static getRandomPrompt = async genre => {
-    let response
-
-    if (genre === 'any') {
-      response = await fetch(baseUrl + '/prompts/any')
-    } else {
-      response = await fetch(baseUrl + `/prompts/${genre}`)
-    }
-    const prompt = await response.json()
-    return prompt
-  }
-
-
-  
 }
 
 export default ApiHelper
