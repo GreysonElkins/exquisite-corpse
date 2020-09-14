b736b13f448a9ae4b7bd913abbf41dc7a637d25c
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-14 16:00:56 -0600

    Add spans for first letter and adjust auto formatting #43

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -6,25 +6,40 @@ import { Link } from 'react-router-dom'
 const Instructions = ( { displayHistory, showHistory } ) => {
   return (
     <section className='Instructions-sec'>
-    <h2 className='Instructions-header-text'>Rules of Play</h2>
+      <h2 className='Instructions-header-text'>
+        <span className='first-letter'>R</span>ules of Play
+      </h2>
       <div className='game-instructions'>
         <ul>
-          <li>Exquisite Corpse is a collaborative creative writing game where users start or continue shared stories, 
-              only being able to read the last line of the previous author's submission. The result is a fun collection of writing styles all making up one story.</li>
+          <li>
+            Exquisite Corpse is a collaborative creative writing game where
+            users start or continue shared stories, only being able to read the
+            last line of the previous author's submission. The result is a fun
+            collection of writing styles all making up one story.
+          </li>
           <ol>
             <li>Select a new prompt or continue one of the stories below</li>
-            <li>Start your section of the story from the prompt or the last line of the previous author</li>
-            <li>Be creative and write fast (you'll have a limited amount of time!)</li>
+            <li>
+              Start your section of the story from the prompt or the last line
+              of the previous author
+            </li>
+            <li>
+              Be creative and write fast (you'll have a limited amount of time!)
+            </li>
           </ol>
         </ul>
       </div>
-      <button className='show-history-button' onClick={displayHistory}>History of Exquisite Corpse</button>
+      <button className='show-history-button' onClick={displayHistory}>
+        History of Exquisite Corpse
+      </button>
       {showHistory ? <GameHistory displayHistory={displayHistory} /> : null}
-      <Link to='/story-setup' className='start-new-story-link'>Start a new story</Link>
+      <Link to='/story-setup' className='start-new-story-link'>
+        Start a new story
+      </Link>
       <h2>OR</h2>
       <h3 className='text'>Continue a story:</h3>
     </section>
-  )
+  );
 }
   
   export default Instructions
