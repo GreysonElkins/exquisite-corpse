134affe321e08b003f17a97074fabb3aa10a7ebd
Nick Hart <nick.hart.des@gmail.com>
2020-09-15 17:17:09 -0500

    Add conditional render for Sign Out button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,28 +2,35 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './Header.scss'
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <header className='header'>
-      <Link to='/'>
-        <h1 className='title'>
-          <span className='first-letter'>E</span>xquisite{' '}
-          <span className='first-letter'>C</span>orpse
+    <header className="header">
+      <Link to="/">
+        <h1 className="title">
+          <span className="first-letter">E</span>xquisite{" "}
+          <span className="first-letter">C</span>orpse
         </h1>
       </Link>
-      <nav className='navbar'>
-        <NavLink exact to='/' className='nav' activeClassName='selected'>
-          / Home / 
+      <nav className="navbar">
+        <NavLink exact to="/" className="nav" activeClassName="selected">
+          / Home /
         </NavLink>
-        <NavLink to='/story-setup' className='nav' activeClassName='selected'>
-         /  New Story /
+        <NavLink to="/story-setup" className="nav" activeClassName="selected">
+          / New Story /
         </NavLink>
-        <NavLink to='/library' className='nav' activeClassName='selected'>
+        <NavLink to="/library" className="nav" activeClassName="selected">
           / Library /
         </NavLink>
-        <NavLink to='/login' className='nav' activeClassName='selected'>
-         / Sign In /
-        </NavLink>
+        {props.currentUser.name && (
+          <NavLink className="nav" activeClassName="selected">
+            / Sign Out /
+          </NavLink>
+        )}
+        {!props.currentUser.name && (
+          <NavLink to="/login" className="nav" activeClassName="selected">
+            / Sign In /
+          </NavLink>
+        )}
       </nav>
     </header>
   );
