bde4f692c83bf2696686916142ab6638ca21f8a2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-14 22:53:00 -0600

    Create bodybuilder method and onclick api request

--- src/components/StoryEditFooter/StoryEditFooter.js
+++ src/components/StoryEditFooter/StoryEditFooter.js
@@ -1,26 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import TimerDisplay from "../TimerDisplay/TimerDisplay";
+import ApiHelper from '../../ApiHelper/ApiHelper'
 import "../StoryEditFooter/StoryEditFooter.scss";
+import ApiHelper from "../../ApiHelper/ApiHelper";
 
-const StoryEditFooter = ({ disableStoryInput, isPublishable }) => {
+const StoryEditFooter = ({ disableStoryInput, textInputs, story, author }) => {
+
+  const bodyBuilder = () => {
+    const body = {
+      contributions: textInputs.story,
+      contributors: author.id
+    }
+    if (!story.id) {
+      body.title = textInputs.story
+      body.prompt = story.prompt.id
+    }
+    return body
+  }
+
+
+
   return (
     <section id="story-edit-footer">
       <TimerDisplay disableStoryInput={disableStoryInput} />
       <Link to="/">
-        <button type="button" id="post-button">
-          Pass it on
+        <button 
+          type="button" 
+          id="post-button" 
+          onClick={() => {
+            const body = bodyBuilder()
+            !story.id ? ApiHelper.postStory(body) : ApiHelper.editStory(body)
+          }}
+        >
+          {!story.id ? "Continue Story" : "Start Story"}
         </button>
       </Link>
-      {isPublishable && (
+      {story.id && (
         <Link to="/">
-          <button type="button" id="publish-button">
-            Publish it
+          <button
+            type="button" 
+            id="publish-button"
+            onClick={() => {
+              const body = bodyBuilder()
+              body.is_complete = true
+              ApiHelper.editStory(body)
+            }}
+          >
+            Finish Story
           </button>
         </Link>
       )}
     </section>
-  );
+  )
 }
 
 export default StoryEditFooter
