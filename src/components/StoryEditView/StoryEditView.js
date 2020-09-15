a6a5e8fd821a831df0c8fc56e7deb63ce7602411
Carly Clift <carlymclift@gmail.com>
2020-09-14 20:30:03 -0600

    Style login input areas

--- src/components/StoryEditView/StoryEditView.js
+++ src/components/StoryEditView/StoryEditView.js
@@ -12,7 +12,6 @@ class StoryEditView extends Component {
 
   updateText = e => {
     this.setState({ [e.target.id]: e.target.value })
-    console.log(this.state)
   }
 
   disableStoryInput = () => {
