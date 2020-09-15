d54adc60369702727019459ebe22642c9959229a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-14 18:43:46 -0600

    Add div for sidenav to make display horizontal #43

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -6,10 +6,10 @@ import { Link } from 'react-router-dom'
 const Instructions = ( { displayHistory, showHistory } ) => {
   return (
     <section className='Instructions-sec'>
+      <div className='game-instructions'>
       <h2 className='Instructions-header-text'>
         <span className='first-letter'>R</span>ules of Play
       </h2>
-      <div className='game-instructions'>
         <p>
           <b><span className='first-letter'>☞</span> <span className='first-letter'>E</span>xquisite Corpse is a
           collaborative creative writing game. Participants take turns writing shared stories, 
@@ -19,7 +19,7 @@ const Instructions = ( { displayHistory, showHistory } ) => {
         </p>
         <ul>
           <ol>
-            <li>Begin a new story or continue one of the stories below</li>
+            <li>Begin a new story or continue one of the stories to the right <span className='first-letter'>☞</span></li>
             <li>
               Start your section of the story from a prompt or the last line
               of the previous author
@@ -30,15 +30,17 @@ const Instructions = ( { displayHistory, showHistory } ) => {
           </ol>
         </ul>
       </div>
-      <button className='show-history-button' onClick={displayHistory}>
-        <span className='first-letter'>☞</span> History of Exquisite Corpse
-      </button>
-      {showHistory ? <GameHistory displayHistory={displayHistory} /> : null}
-      <Link to='/story-setup' className='start-new-story-link'>
-        <span className='first-letter'>☞</span> Start a new story
-      </Link>
-      <h2>OR</h2>
-      <h3 className='text'>Continue a story:</h3>
+      <div className='sidenav'>
+        <button className='show-history-button' onClick={displayHistory}>
+          <span className='first-letter'>☞</span> History of Exquisite Corpse
+        </button>
+        {showHistory ? <GameHistory displayHistory={displayHistory} /> : null}
+        <Link to='/story-setup' className='start-new-story-link'>
+          <span className='first-letter'>☞</span> Start a new story
+        </Link>
+        <h2 className='or'>OR</h2>
+        <h3 className='text'>Continue a story:</h3>
+      </div>
     </section>
   )
 }
