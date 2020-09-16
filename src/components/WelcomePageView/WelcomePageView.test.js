c71415a6a1e169ac088c4c8f33c497d826f5ae71
Carly Clift <carlymclift@gmail.com>
2020-09-15 19:29:18 -0600

    Import correct jest

--- src/components/WelcomePageView/WelcomePageView.test.js
+++ src/components/WelcomePageView/WelcomePageView.test.js
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 import testData from "../../assets/testData/testData";
+import '@testing-library/jest-dom'
 
 describe('WelcomePageView', () => {
 
